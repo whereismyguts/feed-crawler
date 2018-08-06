467f0fb85754592d1098ac57733fff0526db890e
karmanov anton <@KARMANOV>
2018-08-06 11:20:02 +0300

    port

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     try:
         # Override default port for `runserver` command
         from django.core.management.commands.runserver import Command as runserver
-        runserver.default_port = "8555"
+        runserver.default_port = "5000"
         from django.core.management import execute_from_command_line
         
     except ImportError as exc:
